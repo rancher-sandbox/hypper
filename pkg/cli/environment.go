7309becd2d365660119f20006659395696584cbc
Christian Richter <crichter@suse.com>
2021-01-21 10:57:41 +0100

    Add tests for cli package
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- pkg/cli/environment.go
+++ pkg/cli/environment.go
@@ -1,33 +1,38 @@
 package cli
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 
 	"github.com/spf13/pflag"
 )
 
+// EnvSettings describes all of the environment settings.
 type EnvSettings struct {
 	Debug    bool
 	NoColors bool
 }
 
+// New is a constructor of EnvSettings
 func New() *EnvSettings {
 	env := &EnvSettings{}
 	env.Debug, _ = strconv.ParseBool(os.Getenv("HYPPER_DEBUG"))
-	env.NoColors, _ = strconv.ParseBool(envOr("HYPPER_COLORS", "false"))
+	env.NoColors, _ = strconv.ParseBool(os.Getenv("HYPPER_COLORS"))
 
 	return env
 }
 
+// AddFlags binds flags to the given flagset.
 func (s *EnvSettings) AddFlags(fs *pflag.FlagSet) {
 	fs.BoolVarP(&s.Debug, "debug", "d", s.Debug, "enable verbose output")
 	fs.BoolVarP(&s.NoColors, "no-colors", "n", s.NoColors, "disable colors")
 }
 
-func envOr(name, def string) string {
-	if v, ok := os.LookupEnv(name); ok {
-		return v
+// EnvVars gives you a string map of the environment variables
+func (s *EnvSettings) EnvVars() map[string]string {
+	return map[string]string{
+		"HYPPER_DEBUG":    fmt.Sprint(s.Debug),
+		"HYPPER_NOCOLORS": fmt.Sprint(s.NoColors),
 	}
-	return def
 }
--- pkg/cli/environment_test.go
+++ pkg/cli/environment_test.go
@@ -1,9 +1,84 @@
 package cli
 
 import (
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/spf13/pflag"
 )
 
 func TestEnvSettings(t *testing.T) {
-	// TODO: implement me
+	tests := []struct {
+		name string
+
+		// input
+		args    string
+		envvars map[string]string
+
+		// expected values
+		debug    bool
+		noColors bool
+	}{
+		{
+			name:     "defaults",
+			debug:    false,
+			noColors: false,
+		},
+		{
+			name:     "with flags set",
+			args:     "--debug --nocolor",
+			debug:    true,
+			noColors: true,
+		},
+		{
+			name:     "with envvars set",
+			envvars:  map[string]string{"HYPPER_DEBUG": "true", "HYPPER_NOCOLORS": "true"},
+			debug:    true,
+			noColors: true,
+		},
+		{
+			name:     "with args and envvars set",
+			args:     "--debug --nocolor",
+			envvars:  map[string]string{"HYPPER_DEBUG": "false", "HYPPER_NOCOLORS": "false"},
+			debug:    true,
+			noColors: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer resetEnv()()
+
+			for k, v := range tt.envvars {
+				os.Setenv(k, v)
+			}
+
+			flags := pflag.NewFlagSet("testing", pflag.ContinueOnError)
+
+			settings := New()
+			settings.AddFlags(flags)
+			flags.Parse(strings.Split(tt.args, " "))
+
+			if settings.Debug != tt.debug {
+				t.Errorf("expected debug %t, got %t", tt.debug, settings.Debug)
+			}
+		})
+	}
+}
+
+func resetEnv() func() {
+	origEnv := os.Environ()
+
+	// ensure any local envvars do not hose us
+	for e := range New().EnvVars() {
+		os.Unsetenv(e)
+	}
+
+	return func() {
+		for _, pair := range origEnv {
+			kv := strings.SplitN(pair, "=", 2)
+			os.Setenv(kv[0], kv[1])
+		}
+	}
 }
