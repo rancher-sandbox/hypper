175522150e8579cf6654dbedd393d5380a8926bb
Itxaka <igarcia@suse.com>
2021-01-27 14:14:23 +0100

    Add install tests
    Currently because we dont have any custom code, this is mainly
    a 1:1 copy from upstream helm tests, but its still useful to
    make sure that we are using the helm libs properly and not
    breaking stuff when we start to manipulate charts and so on

--- cmd/hypper/install_test.go
+++ cmd/hypper/install_test.go
@@ -5,5 +5,17 @@ import (
 )
 
 func TestInstallCmd(t *testing.T) {
-
+	tests := []cmdTestCase{
+		// Install, base case
+		{
+			name: "basic install",
+			cmd:  "install empty testdata/testcharts/empty",
+		},
+		{
+			name:      "install with no chart specified",
+			cmd:       "install",
+			wantError: true,
+		},
+	}
+	runTestActionCmd(t, tests)
 }
