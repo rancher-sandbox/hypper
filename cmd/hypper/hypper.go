766dfec7eb6788bc16aea8e4e472e24476304ada
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2021-01-27 11:24:47 +0100

    Initialize actionConfig in cobra.OnInitialize()
    Signed-off-by: Christian Richter <crichter@suse.com>

--- cmd/hypper/hypper.go
+++ cmd/hypper/hypper.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/kyokomi/emoji/v2"
 	"github.com/mattfarina/hypper/pkg/cli"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 	helmAction "helm.sh/helm/v3/pkg/action"
+	kubefake "helm.sh/helm/v3/pkg/kube/fake"
+	"helm.sh/helm/v3/pkg/release"
+	"helm.sh/helm/v3/pkg/storage/driver"
 )
 
 var settings = cli.New()
@@ -53,11 +59,55 @@ func main() {
 	}
 
 	cobra.OnInitialize(func() {
+		helmDriver := os.Getenv("HELM_DRIVER")
+		if err := actionConfig.Init(settings.HelmSettings.RESTClientGetter(), settings.HelmSettings.Namespace(), helmDriver, debug); err != nil {
+			log.Fatal(err)
+		}
+		if helmDriver == "memory" {
+			loadReleasesInMemory(actionConfig)
+		}
 	})
 
 	if err := cmd.Execute(); err != nil {
 		debug("%+v", err)
 		os.Exit(1)
 	}
-	fmt.Println((esPrint(":wave: Welcome to") + esPrintf(" %s", blue("Hypper"))))
+}
+
+// This function loads releases into the memory storage if the
+// environment variable is properly set.
+func loadReleasesInMemory(actionConfig *helmAction.Configuration) {
+	filePaths := strings.Split(os.Getenv("HELM_MEMORY_DRIVER_DATA"), ":")
+	if len(filePaths) == 0 {
+		return
+	}
+
+	store := actionConfig.Releases
+	mem, ok := store.Driver.(*driver.Memory)
+	if !ok {
+		// For an unexpected reason we are not dealing with the memory storage driver.
+		return
+	}
+
+	actionConfig.KubeClient = &kubefake.PrintingKubeClient{Out: ioutil.Discard}
+
+	for _, path := range filePaths {
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			log.Fatal("Unable to read memory driver data", err)
+		}
+
+		releases := []*release.Release{}
+		if err := yaml.Unmarshal(b, &releases); err != nil {
+			log.Fatal("Unable to unmarshal memory driver data: ", err)
+		}
+
+		for _, rel := range releases {
+			if err := store.Create(rel); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	// Must reset namespace to the proper one
+	mem.SetNamespace(settings.HelmSettings.Namespace())
 }
