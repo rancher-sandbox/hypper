90c30c173881921d2c527eb6b421c9daf6531829
Christian <crichter@suse.com>
2021-01-28 10:14:28 +0100

    Add emoji support

--- pkg/cli/environment_test.go
+++ pkg/cli/environment_test.go
@@ -9,63 +9,6 @@ import (
 	"github.com/spf13/pflag"
 )
 
-func TestEmojis(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    string
-		expected string
-	}{
-		{
-			name:     "plain string",
-			input:    "this is a plain string",
-			expected: "this is a plain string",
-		},
-		{
-			name:     "plain utf-8 string",
-			input:    "\u2000-\u3300",
-			expected: "\u2000-\u3300",
-		},
-		{
-			name:     "pure emoji string",
-			input:    ":pizza::beer:",
-			expected: "",
-		},
-		{
-			name:     "mixed string",
-			input:    ":pizza: beer",
-			expected: " beer",
-		},
-		{
-			name:     "weird edge cases",
-			input:    ":pizza: :beer: :pizza beer:",
-			expected: "  :pizza beer:",
-		},
-		{
-			name:     "double colon",
-			input:    ":: test",
-			expected: ":: test",
-		},
-		{
-			name:     "double colon emoji mixup",
-			input:    "::pizza::",
-			expected: "::",
-		},
-		{
-			name:     "do not touch native utf-8 inside colons",
-			input:    ":\u2000-\u3300:",
-			expected: ":\u2000-\u3300:",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			s := RemoveEmojiFromString(tt.input)
-			if tt.expected != s {
-				t.Errorf("expected %s got %s", tt.expected, s)
-			}
-		})
-	}
-}
-
 func TestEnvSettings(t *testing.T) {
 	tests := []struct {
 		name string
---
+++ pkg/eyecandy/emoji.go
@@ -0,0 +1,26 @@
+package eyecandy
+
+import (
+	"fmt"
+	"github.com/kyokomi/emoji/v2"
+	"regexp"
+)
+
+func ESPrintf(emojisDisabled bool, format string, v ...interface{}) string {
+	if emojisDisabled {
+		return fmt.Sprintf(removeEmojiFromString(format), v...)
+	}
+	return emoji.Sprintf(format, v...)
+}
+
+func ESPrint(emojisDisabled bool, s string) string {
+	if emojisDisabled {
+		return fmt.Sprint(removeEmojiFromString(s))
+	}
+	return emoji.Sprint(s)
+}
+
+func removeEmojiFromString(s string) string {
+	re := regexp.MustCompile(`:[a-zA-Z0-9-_+]+?:`)
+	return re.ReplaceAllString(s, "")
+}
