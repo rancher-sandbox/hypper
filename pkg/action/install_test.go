0ba641757d5f57f6657b03f5a4744a634e84f81d
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2021-02-23 15:06:30 +0100

    fix: Make pkg/action/action_test.go.with*() composable
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- pkg/action/install_test.go
+++ pkg/action/install_test.go
@@ -44,7 +44,7 @@ func TestSetNamespace(t *testing.T) {
 
 	// hypper annotations have priority over fallback annotations
 	instAction = installAction(t)
-	chart = buildChart(withHypperAnnotations())
+	chart = buildChart(withHypperAnnotations(), withFallbackAnnotations())
 	instAction.SetNamespace(chart, "defaultns")
 	is.Equal("hypper", instAction.Namespace)
 
@@ -78,7 +78,7 @@ func TestName(t *testing.T) {
 
 	// hypper annotations have priority over fallback annotations
 	instAction = installAction(t)
-	chart = buildChart(withHypperAnnotations())
+	chart = buildChart(withHypperAnnotations(), withFallbackAnnotations())
 	name, err = instAction.Name(chart, []string{"chart-uri"})
 	if err != nil {
 		t.Fatal(err)
