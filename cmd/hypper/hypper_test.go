025010c5eac9ad0429ff3c20cc2550a2937c6e49
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2021-05-20 15:15:06 +0200

    fix: Funnel all log output to buf in cmd tests
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- cmd/hypper/hypper_test.go
+++ cmd/hypper/hypper_test.go
@@ -122,9 +122,13 @@ func executeActionCommandStdinC(store *storage.Storage, in *os.File, cmd string)
 	// create our own Logger that satisfies impl/cli.Logger, but with a buffer for tests
 	buf := new(bytes.Buffer)
 	logger := logcli.NewStandard()
+	logger.TraceOut = buf
+	logger.DebugOut = buf
 	logger.InfoOut = buf
 	logger.WarnOut = buf
 	logger.ErrorOut = buf
+	logger.FatalOut = buf
+	logger.PanicOut = buf
 	log.Current = logger
 
 	root, err := newRootCmd(actionConfig, log.Current, args)
@@ -189,8 +193,13 @@ func executeCommandStdinC(cmd string) (*cobra.Command, string, error) {
 	// create our own Logger that satisfies impl/cli.Logger, but with a buffer for tests
 	buf := new(bytes.Buffer)
 	logger := logcli.NewStandard()
+	logger.TraceOut = buf
+	logger.DebugOut = buf
 	logger.InfoOut = buf
+	logger.WarnOut = buf
 	logger.ErrorOut = buf
+	logger.FatalOut = buf
+	logger.PanicOut = buf
 	log.Current = logger
 
 	root, err := newRootCmd(actionConfig, log.Current, args)
