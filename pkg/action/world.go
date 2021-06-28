f5082a43aec4fc6dd3e61e99132d40a8fcafdc2b
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2021-06-28 14:19:37 +0200

    feat: Correctly implement action.BuildWorld() with repo entries
    Co-authored-by: Christian Richter <crichter@suse.com>
    Co-authored-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>
    Signed-off-by: Christian Richter <crichter@suse.com>

--- pkg/action/world.go
+++ pkg/action/world.go
@@ -17,134 +17,132 @@ limitations under the License.
 package action
 
 import (
-	"fmt"
 	"path/filepath"
 
 	"github.com/Masterminds/log-go"
+	"gopkg.in/yaml.v2"
+
 	pkg "github.com/rancher-sandbox/hypper/internal/package"
 	solver "github.com/rancher-sandbox/hypper/internal/solver"
-	"github.com/rancher-sandbox/hypper/pkg/chart"
 	"github.com/rancher-sandbox/hypper/pkg/cli"
 	"github.com/rancher-sandbox/hypper/pkg/repo"
 
-	helmAction "helm.sh/helm/v3/pkg/action"
-	"helm.sh/helm/v3/pkg/chart/loader"
-	helmLoader "helm.sh/helm/v3/pkg/chart/loader"
-	// helmDownloader "helm.sh/helm/v3/pkg/downloader"
+	helmChart "helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/helmpath"
 	"helm.sh/helm/v3/pkg/release"
 	helmRepo "helm.sh/helm/v3/pkg/repo"
 )
 
 // FIXME assume all charts come from just 1 repo. We will generalize later.
-// TODO add chart.Hash into index file, to not need to pull hypper charts.
-// Note that we will still need to pull helm charts to calculate its chart.Hash
-func BuildWorld(s *solver.Solver, repositories []*helmRepo.Entry,
+func BuildWorld(pkgdb *solver.PkgDB, repositories []*helmRepo.Entry,
 	releases []*release.Release, toModify []*pkg.Pkg,
 	settings *cli.EnvSettings, logger log.Logger) (err error) {
 
-	// add repos to db
-	// for all repos:
+	// concatenate all index entries from all repositories:
+	repoEntries := make(map[string][]*helmRepo.ChartVersion)
 	for _, r := range repositories {
-		fmt.Printf("repository: %v\n", r)
 		idxFilepath := filepath.Join(settings.RepositoryCache, helmpath.CacheIndexFile(r.Name))
-		// obtain repo index file from cache
+		// obtain repo index file from cache:
 		index, err := repo.LoadIndexFile(idxFilepath)
 		if err != nil {
 			return err
 		}
-
-		for _, repoEntries := range index.Entries {
-			fmt.Printf("repoEntries: %v\n", repoEntries)
-			// for all chart entries in the repo:
-			for _, chartVersion := range repoEntries {
-				fmt.Printf("chartVersion: %v\n", chartVersion)
-				// obtain the chart (needed for pkg.ChartHash)
-				chart, err := loader.Load(filepath.Join(r.URL, chartVersion.URLs[0]))
-				if err != nil {
-					fmt.Println(err)
-					return err
-				}
-				fmt.Printf("chart: %v\n", chart)
-
-				// add chart to db
-				ns := GetNamespace(chart, "") //TODO figure out the default ns for bare helm charts, and honour kubectl ns and flag
-				p, err := pkg.NewPkgFromChart(chart, chart.Name(), ns, pkg.Unknown)
-				if err != nil {
-					return err
-				}
-				s.PkgDB.Add(p)
-				fmt.Printf("package from repo: %v\n", p.GetFingerPrint())
-			}
+		for chrtName, chrtVers := range index.Entries {
+			// TODO this overrides other repos, if charts are in both
+			repoEntries[chrtName] = chrtVers
 		}
 	}
 
-	fmt.Println("Printing db after adding repos")
-	for i := 1; i <= s.PkgDB.Size(); i++ { // IDs start with 1
-		p := s.PkgDB.GetPackageByPbID(i)
-		fmt.Printf("Package: %s  Currentstate: %v   DesiredState: %v Version: %v \n", p.Name, p.CurrentState, p.DesiredState, p.Version)
+	// add repos to db
+	// for all chart entries in repos:
+	for chrtName, chrtVersions := range repoEntries {
+		// for all the versions of a chart:
+		for _, chrtVer := range chrtVersions {
+
+			// add chart to db:
+			ns := GetNamespaceFromAnnot(chrtVer.Annotations, settings.Namespace()) //TODO figure out the default ns for bare helm charts, and honour kubectl ns and flag
+			name := GetNameFromAnnot(chrtVer.Annotations, chrtVer.Metadata.Name)   // TODO default name for helm repos
+			p := pkg.NewPkg(name, chrtVer.Version, ns, pkg.Unknown, pkg.Unknown)
+
+			// unmarshal dependencies:
+			sharedDepsYaml, _ := chrtVer.Annotations["hypper.cattle.io/shared-dependencies"]
+			var sharedDeps []*helmChart.Dependency
+			// unmarshalling Helm's Dependency because gopkg.in/yaml.v2 doesn't do composite types
+			if err := yaml.UnmarshalStrict([]byte(sharedDepsYaml), &sharedDeps); err != nil {
+				log.Errorf("Chart.yaml metadata is malformed for repo entry \"%s\", \"%s\"\n", chrtName, chrtVer)
+				return err
+			}
+
+			// for all deps in chrtVer.Annotations, find the dep in repo entries
+			// to obtain its default ns:
+			for _, dep := range sharedDeps {
+				// find dependency:
+				depChrtVer, ok := repoEntries[dep.Name]
+				if !ok {
+					log.Warnf("Dependency \"%s\" not found in repos, continuing", dep.Name)
+				}
+
+				// TODO each version can have a different default ns
+				// Iterate through all depChrtVer
+
+				//   obtain default ns of dep
+				depNS := GetNamespaceFromAnnot(depChrtVer[0].Annotations, "") //TODO figure out the default ns for bare helm charts, and honour kubectl ns and flag
+				depName := GetNameFromAnnot(depChrtVer[0].Annotations, "")    // TODO default name for helm repos
+
+				//add relation to pkg
+				p.DependsRel = append(p.DependsRel, &pkg.PkgRel{
+					BaseFingerprint: pkg.CreateBaseFingerPrint(depName, depNS),
+					SemverRange:     dep.Version,
+				})
+			}
+
+			// unmarshal optional dependencies:
+			optSharedDepsYaml, _ := chrtVer.Annotations["hypper.cattle.io/optional-dependencies"]
+			var optSharedDeps []*helmChart.Dependency
+			// unmarshalling Helm's Dependency because gopkg.in/yaml.v2 doesn't do composite types
+			if err := yaml.UnmarshalStrict([]byte(optSharedDepsYaml), &optSharedDeps); err != nil {
+				log.Errorf("Chart.yaml metadata is malformed for repo entry \"%s\", \"%s\"\n", chrtName, chrtVer)
+				return err
+			}
+
+			// for all deps in chrtVer.Annotations, find the dep in repo entries
+			// to obtain its default ns:
+			for _, dep := range optSharedDeps {
+				// find dependency:
+				depChrtVer, ok := repoEntries[dep.Name]
+				if !ok {
+					log.Warnf("Dependency \"%s\" not found in repos, continuing", dep.Name)
+				}
+
+				// TODO each version can have a different default ns
+				// Iterate through all depChrtVer
+
+				//   obtain default ns of dep
+				depNS := GetNamespaceFromAnnot(depChrtVer[0].Annotations, "") //TODO figure out the default ns for bare helm charts, and honour kubectl ns and flag
+				depName := GetNameFromAnnot(depChrtVer[0].Annotations, "")    // TODO default name for helm repos
+
+				//add relation to pkg
+				p.DependsRel = append(p.DependsOptionalRel, &pkg.PkgRel{
+					BaseFingerprint: pkg.CreateBaseFingerPrint(depName, depNS),
+					SemverRange:     dep.Version,
+				})
+			}
+
+			pkgdb.Add(p)
+		}
 	}
 
 	// add releases to db
 	for _, r := range releases {
-		p, err := pkg.NewPkgFromRelease(r)
-		if err != nil {
-			return err
-		}
-		s.PkgDB.Add(p)
+		// TODO pull deps if they aren't present in repos
+		p := pkg.NewPkg(r.Name, r.Chart.Metadata.Version, r.Namespace, pkg.Present, pkg.Unknown)
+		pkgdb.Add(p)
 	}
 
 	// add toModify to db
 	for _, p := range toModify {
-		s.PkgDB.Add(p)
+		pkgdb.Add(p)
 	}
 
-	// create dependency relations in all packages:
-	for i := 1; i <= s.PkgDB.Size(); i++ { // IDs start with 1
-		p := s.PkgDB.GetPackageByPbID(i)
-		createDependencyRelations(p, settings, logger)
-	}
-
-	return nil
-}
-
-func createDependencyRelations(p *pkg.Pkg, settings *cli.EnvSettings, logger log.Logger) error {
-
-	// don't check error, dependencies come from repo, they are correctly formed
-	sharedDeps, _ := chart.GetSharedDeps(p.Chart, logger)
-
-	for _, dep := range sharedDeps {
-		// from chart -> obtain list of deps -> obtain default
-		// ns,version,release, and build relation.
-
-		// pull chart:
-		chartPathOptions := helmAction.ChartPathOptions{}
-		chartPathOptions.RepoURL = dep.Repository
-		cp, err := chartPathOptions.LocateChart(dep.Name, settings.EnvSettings)
-		if err != nil {
-			return err
-		}
-		depChart, err := helmLoader.Load(cp)
-		if err != nil {
-			return err
-		}
-
-		// Obtain fingerprint and semver for relation:
-		depNS := GetNamespace(depChart, "") //TODO figure out the default ns for bare helm charts, and honour kubectl ns and flag
-		baseFP := pkg.CreateBaseFingerPrint(depChart.Name(), depNS)
-
-		// build relation:
-		if dep.IsOptional {
-			p.DependsOptionalRel = append(p.DependsOptionalRel, &pkg.PkgRel{
-				BaseFingerprint: baseFP,
-				SemverRange:     dep.Version,
-			})
-		} else {
-			p.DependsRel = append(p.DependsRel, &pkg.PkgRel{
-				BaseFingerprint: baseFP,
-				SemverRange:     dep.Version,
-			})
-		}
-	}
 	return nil
 }
