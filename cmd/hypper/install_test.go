7001b2157a5144e6faaf002b63f0930060f6034a
Matt Farina <matt.farina@suse.com>
2021-08-24 11:18:44 -0400

    Fixing location of repo cache and config in install tests
    Closes #189
    
    Signed-off-by: Matt Farina <matt.farina@suse.com>

--- cmd/hypper/install_test.go
+++ cmd/hypper/install_test.go
@@ -17,10 +17,16 @@ limitations under the License.
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestInstallCmd(t *testing.T) {
+
+	repoCache := "testdata/testcharts"
+
+	repoConfig := repoCache + "/repositories.yaml"
+
 	tests := []cmdTestCase{
 		// Install, no chart specified
 		{
@@ -32,63 +38,63 @@ func TestInstallCmd(t *testing.T) {
 		// Install, name and namespace as args
 		{
 			name:   "install, name and ns as args",
-			cmd:    "install zeppelin testdata/testcharts/hypper-annot -n led --no-shared-deps",
+			cmd:    fmt.Sprintf("install zeppelin testdata/testcharts/hypper-annot -n led --no-shared-deps --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-name-ns-args.txt",
 		},
 		// Install, name and namespace as args, no create ns
 		{
 			name:   "install, name and ns as args, don't create ns",
-			cmd:    "install purple testdata/testcharts/hypper-annot --namespace deep --no-create-namespace --no-shared-deps",
+			cmd:    fmt.Sprintf("install purple testdata/testcharts/hypper-annot --namespace deep --no-create-namespace --no-shared-deps --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-no-create-namespace.txt",
 			// wantError: true, there's no error, the client allows targetting any ns
 		},
 		// Install, hypper annot have priority over fallback annot
 		{
 			name:   "install, hypper annot have priority over fallback annot",
-			cmd:    "install testdata/testcharts/hypper-annot --no-shared-deps",
+			cmd:    fmt.Sprintf("install testdata/testcharts/hypper-annot --no-shared-deps --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-hypper-annot.txt",
 		},
 		// Install, fallback annotations
 		{
 			name:   "install, fallback annotations",
-			cmd:    "install testdata/testcharts/fallback-annot --no-shared-deps",
+			cmd:    fmt.Sprintf("install testdata/testcharts/fallback-annot --no-shared-deps --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-fallback-annot.txt",
 		},
 		// Install, annotations have priority over default name from Chart.yml
 		{
 			name:   "install, annot have priority over default name from Chart.yml",
-			cmd:    "install testdata/testcharts/hypper-annot --no-shared-deps",
+			cmd:    fmt.Sprintf("install testdata/testcharts/hypper-annot --no-shared-deps --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-hypper-annot.txt",
 		},
 		// Install, no name or annotations specified
 		{
 			name:   "install, with no name or annot specified",
-			cmd:    "install testdata/testcharts/vanilla-helm --no-shared-deps",
+			cmd:    fmt.Sprintf("install testdata/testcharts/vanilla-helm --no-shared-deps --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-no-name-or-annot.txt",
 		},
 		// Install, with shared deps
 		{
 			name:   "install, with shared deps",
-			cmd:    "install testdata/testcharts/shared-deps",
+			cmd:    fmt.Sprintf("install testdata/testcharts/shared-deps --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-with-shared-deps.txt",
 		},
 		// Install, with shared deps out-of-range
 		{
 			name:      "install, with shared deps out-of-range",
-			cmd:       "install testdata/testcharts/shared-deps-out-of-range",
+			cmd:       fmt.Sprintf("install testdata/testcharts/shared-deps-out-of-range --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden:    "output/install-with-shared-deps-out-of-range.txt",
 			wantError: true,
 		},
 		// Install, with all optional shared deps
 		{
 			name:   "install, with all optional shared deps",
-			cmd:    "install testdata/testcharts/shared-and-optional-deps --optional-deps all",
+			cmd:    fmt.Sprintf("install testdata/testcharts/shared-and-optional-deps --optional-deps all --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-with-all-optional-deps.txt",
 		},
 		// Install, skip optional shared deps
 		{
 			name:   "install, skip optional shared deps",
-			cmd:    "install testdata/testcharts/shared-and-optional-deps --optional-deps none",
+			cmd:    fmt.Sprintf("install testdata/testcharts/shared-and-optional-deps --optional-deps none --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-skip-all-optional-deps.txt",
 		},
 
@@ -105,7 +111,7 @@ func TestInstallCmd(t *testing.T) {
 		// dry-run, with all optional shared deps
 		{
 			name:   "install dry-run, with all optional shared deps",
-			cmd:    "install testdata/testcharts/shared-and-optional-deps --optional-deps all --dry-run",
+			cmd:    fmt.Sprintf("install testdata/testcharts/shared-and-optional-deps --optional-deps all --dry-run --repository-config %s --repository-cache %s", repoConfig, repoCache),
 			golden: "output/install-dry-run-with-all-optional-deps.txt",
 		},
 	}
